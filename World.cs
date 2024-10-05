245931996de74894d88d921b7ba238391497f659
Anselm Brehme <development.anselm@breh.me>
2024-10-06 01:42:05 +0200

    spawn one each tick

--- World.cs
+++ World.cs
@@ -5,38 +5,61 @@ public partial class World : Node2D
 {
     [Export] public PackedScene particlePrefab;
 
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
+    private int players = 1;
+    public int totalMass = 0;
+    public int maxMass = 1000000;
+    public int i = 0;
+    
+    private RandomNumberGenerator random = new();
+
+
+
+    public int spawnRandomParticle()
     {
         var spawnRange = GetViewport().GetVisibleRect().Size;
-        var random = new RandomNumberGenerator();
-        // TODO only on server
-        
-        for (int i = 0; i < 1000; i++)
-        {
-            var particle = particlePrefab.Instantiate();
-            var spawnPos = new Vector2(
-                random.RandfRange(0f, spawnRange.X),
-                random.RandfRange(0f, spawnRange.Y));
+
+        var particle = particlePrefab.Instantiate();
+        var spawnPos = new Vector2(
+            random.RandfRange(0f, spawnRange.X),
+            random.RandfRange(0f, spawnRange.Y)
+            // random.RandfRange(50f, 50f)
+        );
             
+        // TODO place particles outside of player range
+        // TODO respect other particles space?
       
 
-            ((Node2D)particle).GlobalPosition = spawnPos;
-            var sprite = particle.GetNode<Sprite2D>("Sprite2D");
-            var randScale = random.RandfRange(10f, 30f);
-            sprite.Scale = new Vector2(randScale, randScale);
+        ((Node2D)particle).GlobalPosition = spawnPos;
+        var sprite = particle.GetNode<Sprite2D>("Sprite2D");
+        var randScale = random.RandfRange(30f, 150f);
+        // var randScale = random.RandfRange(150f, 150f);
+        sprite.Scale = new Vector2(randScale, randScale);
 
-            var color = Color.FromHsv(random.RandfRange(0, 1f), 1f, 1f, 1f);
-            (sprite.Material as ShaderMaterial).SetShaderParameter("color", color);
-            // GD.Print($"spawn at {spawnPos} {color.H}");
+        var color = Color.FromHsv(random.RandfRange(0, 1f), 1f, 1f, 1f);
+        (sprite.Material as ShaderMaterial).SetShaderParameter("bodyColor", color);
+        (sprite.Material as ShaderMaterial).SetShaderParameter("cellColor", color);
+        // TODO when bigger reduct magnitude
+        (sprite.Material as ShaderMaterial).SetShaderParameter("uSeed", random.RandfRange(0f, 10000f));
+        (sprite.Material as ShaderMaterial).SetShaderParameter("uMagnitude", random.RandfRange(0.01f, 0.19f));
+        (sprite.Material as ShaderMaterial).SetShaderParameter("uFrequency", random.RandfRange(0.5f, 5.5f));
+        // GD.Print($"spawn {i++} at {spawnPos} {color.H}");
 
 
-            AddChild(particle);
+        AddChild(particle);
+        return (int)(3 * (randScale * randScale) / 4);
+    }
+    
+    public void spawnToMaxMass()
+    {
+        if (totalMass < maxMass)
+        {
+            totalMass += spawnRandomParticle();
         }
     }
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
     public override void _Process(double delta)
     {
+        spawnToMaxMass();
     }
 }
