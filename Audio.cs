0e41b71c931490ecb73410ff367d0a13b0bf3a07
Phillip Schichtel <phillip@schich.tel>
2024-10-06 20:24:25 +0200

    background audio and Audio global

---
+++ Audio.cs
@@ -0,0 +1,36 @@
+using System;
+using Godot;
+using GodotPlugins.Game;
+
+namespace LD56;
+
+public partial class Audio : Node
+{
+    private AudioStreamPlayer backgroundMusic = new();
+
+    public float BackgroundVolumeLinear
+    {
+        get => Mathf.DbToLinear(backgroundMusic.VolumeDb);
+        set => backgroundMusic.VolumeDb = Mathf.LinearToDb(value);
+    }
+    
+    public override void _Ready()
+    {
+        StartBackgroundMusic();
+    }
+
+    public void StartBackgroundMusic()
+    {
+        BackgroundVolumeLinear = 0.1f;
+        backgroundMusic.Stream = GD.Load<AudioStream>("res://audio/background-music.mp3");
+        backgroundMusic.Autoplay = true;
+        backgroundMusic.Finished += () => backgroundMusic.Play(); 
+        AddChild(backgroundMusic);
+    }
+
+    public void StopBackgroundMusic()
+    {
+        backgroundMusic.Stop();
+        RemoveChild(backgroundMusic);
+    }
+}
