e304adf04ddd99f86a1fc5bcfaae10de001e4dae
Anselm Brehme <development.anselm@breh.me>
2024-10-06 03:24:31 +0200

    check particle collision

---
+++ Particle.cs
@@ -0,0 +1,59 @@
+using Godot;
+using System;
+
+public partial class Particle : Node2D
+{
+    private bool validSpawn = false;
+    private double aliveTime = 0;
+    public int size;
+
+
+    public void RemoveFromGame()
+    {
+        if (!IsQueuedForDeletion())
+        {
+            var world = (World)GetParent();
+            world.totalMass -= size;
+        }
+
+        QueueFree();
+    }
+
+    public void _on_area_2d_area_entered(Area2D area)
+    {
+        var otherParticle = area.GetParent<Particle>(); // TODO other things that collide?
+        if (validSpawn)
+        {
+            if (!otherParticle.validSpawn)
+            {
+                otherParticle.RemoveFromGame();
+            }
+            else
+            {
+                // TODO how does this happen?
+                // GD.Print("Both are valid?");
+            }
+        }
+        else if (otherParticle.validSpawn)
+        {
+            RemoveFromGame();
+        }
+        else
+        {
+            validSpawn = true;
+            otherParticle.RemoveFromGame();
+        }
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+    {
+        aliveTime += delta;
+        if (aliveTime >= 0.5d)
+        {
+            validSpawn = true;
+        }
+
+        GetNode<Sprite2D>("Sprite2D").Visible = validSpawn;
+    }
+}
--- World.cs
+++ World.cs
@@ -1,5 +1,6 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 public partial class World : Node2D
 {
@@ -7,16 +8,16 @@ public partial class World : Node2D
 
     private int players = 1;
     public int totalMass = 0;
-    public int maxMass = 1000000;
+    public int maxMass = 10000;
     public int i = 0;
-    
-    private RandomNumberGenerator random = new();
 
+    private RandomNumberGenerator random = new();
 
 
     public int spawnRandomParticle()
     {
         var spawnRange = GetViewport().GetVisibleRect().Size;
+        
 
         var particle = particlePrefab.Instantiate();
         var spawnPos = new Vector2(
@@ -24,31 +25,33 @@ public partial class World : Node2D
             random.RandfRange(0f, spawnRange.Y)
             // random.RandfRange(50f, 50f)
         );
-            
+
         // TODO place particles outside of player range
         // TODO respect other particles space?
-      
+
+        
 
         ((Node2D)particle).GlobalPosition = spawnPos;
         var sprite = particle.GetNode<Sprite2D>("Sprite2D");
-        var randScale = random.RandfRange(30f, 150f);
+        var randScale = random.RandfRange(3f, 10f);
         // var randScale = random.RandfRange(150f, 150f);
-        sprite.Scale = new Vector2(randScale, randScale);
+        ((Node2D)particle).Scale = new Vector2(randScale, randScale);
 
-        var color = Color.FromHsv(random.RandfRange(0, 1f), 1f, 1f, 1f);
+        var color = Color.FromHsv(random.RandfRange(0, 1f), 1f, 1f, random.RandfRange(0.2f, 0.4f));
         (sprite.Material as ShaderMaterial).SetShaderParameter("bodyColor", color);
-        (sprite.Material as ShaderMaterial).SetShaderParameter("cellColor", color);
+        (sprite.Material as ShaderMaterial).SetShaderParameter("cellColor", Colors.Black);
+        // (sprite.Material as ShaderMaterial).SetShaderParameter("cellColor", color);
         // TODO when bigger reduct magnitude
         (sprite.Material as ShaderMaterial).SetShaderParameter("uSeed", random.RandfRange(0f, 10000f));
         (sprite.Material as ShaderMaterial).SetShaderParameter("uMagnitude", random.RandfRange(0.01f, 0.19f));
         (sprite.Material as ShaderMaterial).SetShaderParameter("uFrequency", random.RandfRange(0.5f, 5.5f));
         // GD.Print($"spawn {i++} at {spawnPos} {color.H}");
-
-
+        ((Particle)particle).size = (int)(3 * (randScale * randScale) / 4);
+            
         AddChild(particle);
-        return (int)(3 * (randScale * randScale) / 4);
+        return ((Particle)particle).size;
     }
-    
+
     public void spawnToMaxMass()
     {
         if (totalMass < maxMass)
