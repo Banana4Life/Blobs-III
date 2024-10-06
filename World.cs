375115c63f8b3d910c7bb4ab48a49266f2fc3cb6
Anselm Brehme <development.anselm@breh.me>
2024-10-06 21:43:23 +0200

    cam lerp

--- World.cs
+++ World.cs
@@ -11,10 +11,12 @@ public partial class World : Node2D
     private int players = 1;
     public int totalMass = 0;
     [Export] public int maxMass = 1000;
-    [Export] public Vector2 PlayArea = new(1000,1000);
+    [Export] public Vector2 PlayArea = new(1000, 1000);
     public int i = 0;
 
     private RandomNumberGenerator random = new();
+
+    private Player authorityPlayer;
 
 
     public override void _Ready()
@@ -24,7 +26,7 @@ public partial class World : Node2D
     public int spawnRandomParticle()
     {
         var spawnRange = GetViewport().GetVisibleRect().Size;
-        
+
 
         var particle = particlePrefab.Instantiate();
         var spawnPos = new Vector2(
@@ -36,7 +38,6 @@ public partial class World : Node2D
         // TODO place particles outside of player range
         // TODO respect other particles space?
 
-        
 
         ((Node2D)particle).GlobalPosition = spawnPos;
         var sprite = particle.GetNode<Sprite2D>("Sprite2D");
@@ -45,14 +46,14 @@ public partial class World : Node2D
         ((Node2D)particle).Scale = new Vector2(randScale, randScale);
 
         var color = Color.FromHsv(random.RandfRange(0, 1f), 1f, 1f, random.RandfRange(0.2f, 0.4f));
-        
+
         particle.Name = "particle_" + i++;
         ((Particle)particle).Color = color;
         ((Particle)particle).size = (int)(3 * (randScale * randScale) / 4);
         ((Particle)particle).seed = random.RandfRange(0f, 10000f);
         ((Particle)particle).mag = random.RandfRange(0.01f, 0.19f);
         ((Particle)particle).freq = random.RandfRange(0.5f, 5.5f);
-            
+
         AddChild(particle);
         return ((Particle)particle).size;
     }
@@ -68,6 +69,12 @@ public partial class World : Node2D
     // Called every frame. 'delta' is the elapsed time since the previous frame.
     public override void _Process(double delta)
     {
+        if (authorityPlayer != null)
+        {
+            var cam = GetNode<Camera2D>("Camera2D");
+            cam.Position = cam.Position.Lerp(authorityPlayer.Position, 0.1f);
+        }
+
         if (Global.Instance.State is ServerState)
         {
             spawnToMaxMass();
@@ -91,10 +98,9 @@ public partial class World : Node2D
                     RemoveChild(found);
                 }
             }
-
         }
     }
-    
+
     private void SpawnPlayer(PlayerManager.PlayerInfo info)
     {
         GD.Print($"{Multiplayer.GetUniqueId()}: player spawn {info.name} {info.peerId}");
@@ -102,13 +108,12 @@ public partial class World : Node2D
         var player = playerPrefab.Instantiate();
         player.Name = info.peerId.ToString();
         AddChild(player);
-        
-        var spawnPos = new Vector2(50, new Random().Next(50,500));
+
+        var spawnPos = new Vector2(50, new Random().Next(50, 500));
         RpcId(info.peerId, MethodName.initPlayerOnAuthority, info.name, info.peerId, spawnPos, info.size);
         info.alive = true;
-
     }
-    
+
     [Rpc(MultiplayerApi.RpcMode.AnyPeer, CallLocal = true)]
     private void initPlayerOnAuthority(String displayName, long id, Vector2 position, int size)
     {
@@ -116,11 +121,9 @@ public partial class World : Node2D
         existing.GlobalPosition = position;
         existing.PlayerSize = size;
         existing.DisplayName = displayName;
-        GD.Print($"{Multiplayer.GetUniqueId()}: Player {displayName}({id}) init size: {existing.PlayerSize} auth {existing.GetMultiplayerAuthority()}");
-        
-        existing.GetNode<Camera2D>("Camera2D").MakeCurrent();
+        GD.Print(
+            $"{Multiplayer.GetUniqueId()}: Player {displayName}({id}) init size: {existing.PlayerSize} auth {existing.GetMultiplayerAuthority()}");
+
+        authorityPlayer = existing;
     }
-
-
-    
 }
